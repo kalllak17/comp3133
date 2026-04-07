0a8ed8df81df7cd57b89c32e628ee10884f11fac
kalllak17 <kalllak17@gmail.com>
2026-04-07 18:29:57 -0400

    added lab 10

---
+++ lab10_101513972/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  protected readonly title = signal('week13_angular_env');
+
+  constructor() {
+    console.log('Environment: ', environment);
+  }
+}
---
+++ lab10_101513972/src/environments/environment.stage.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://stage.localhost/3000'
+};
